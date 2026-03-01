093eca3eea9f35064f45ae859918d13e83a0f336
Abhay Vishwakarma <hb9868313@gmail.com>
2026-03-01 19:57:30 +0530

    cors origin true kr diya maine

--- server.js
+++ server.js
@@ -21,7 +21,8 @@ startTaskScheduler()
 
 app.use(express.json())
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173" || "https://erp-frontend-snowy-nine.vercel.app/",
+  // origin: process.env.FRONTEND_URL || "http://localhost:5173" || "https://erp-frontend-snowy-nine.vercel.app/",
+  origin: true,
   credentials: true
 }))
 app.use(cookieParser())
